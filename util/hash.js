e8664bd7f53ca1c7968ff121a763ed6e00df4f2e
Alex Fang <deeno@qq.com>
2022-04-24 02:53:13 +0800

    fix: fix hashing default value setter

--- util/hash.js
+++ util/hash.js
@@ -2,8 +2,8 @@ import MD5 from 'md5'
 import FsExtra from 'fs-extra'
 
 function getHash (path = '', opts = {}) {
-  const offset = +opts.offset | 50
-  const limit = +opts.limit | 102400
+  const offset = +opts.offset || 50
+  const limit = +opts.limit || 102400
   const fd = FsExtra.openSync(path, 'r')
   const buf = Buffer.alloc(limit)
   FsExtra.readSync(fd, buf, 0, limit, offset)
