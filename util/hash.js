b6ea4b6dae7a8b74a2ea4465eca7252bd9a5af9b
Alex Fang <deeno@qq.com>
2022-04-24 02:54:20 +0800

    fix: fix hashing strategy to avoid slowing down if file is smaller than buffer size

--- util/hash.js
+++ util/hash.js
@@ -5,8 +5,10 @@ function getHash (path = '', opts = {}) {
   const offset = +opts.offset || 50
   const limit = +opts.limit || 102400
   const fd = FsExtra.openSync(path, 'r')
-  const buf = Buffer.alloc(limit)
-  FsExtra.readSync(fd, buf, 0, limit, offset)
+  const fstat = FsExtra.fstatSync(fd)
+  const bufSize = fstat.size - offset > limit ? limit : fstat.size - offset
+  const buf = Buffer.alloc(bufSize)
+  FsExtra.readSync(fd, buf, 0, bufSize, offset)
   FsExtra.closeSync(fd)
   const md5 = MD5(buf)
 
