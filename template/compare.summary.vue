0fccc8664a75f5084255135363ae13e586f9424a
Alex Fang <deeno@qq.com>
2022-08-23 23:49:54 +0800

    feat: new UI design & interactivity for summary page

--- template/compare.summary.vue
+++ template/compare.summary.vue
@@ -5,57 +5,128 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>文件比对结果</title>
   <link rel="stylesheet" type="text/css" href="https://cdn.bootcdn.net/ajax/libs/element-ui/2.15.6/theme-chalk/index.css">
+  <style type="text/css">
+  #app {
+    display: flex;
+    flex-direction: column;
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    padding: .7rem;
+  }
+  .drawer-wrapper {
+    padding: 0 1rem;
+  }
+  .form-component {
+    width: 100%;
+  }
+  .action-wrapper {
+    flex: none;
+    margin-bottom: .7rem;
+  }
+  .table-wrapper {
+    position: relative;
+    flex: auto;
+  }
+  .table-component {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+  }
+  </style>
 </head>
 <body>
   <div id="app">
-    <el-card>
-      <el-select
-        v-model="state.filterTypes"
-        multiple>
-        <el-option
-          v-for="(item, i) in plugins.typeOptions"
-          :key="i"
-          :label="item.label"
-          :value="item.value">
-        </el-option>
-      </el-select>
-    </el-card>
+    <!-- Popups -->
+    <el-drawer
+      :visible.sync="state.filterDrawerVisible"
+      title="过滤筛选"
+      direction="rtl"
+      size="40%"
+      :wrapperClosable="false"
+      append-to-body
+      modal-append-to-body>
+      <el-form
+        class="drawer-wrapper"
+        size="mini"
+        label-position="left"
+        label-width="100px">
+        <el-form-item label="差异类型">
+          <el-select
+            v-model="state.filterTypes"
+            class="form-component"
+            multiple>
+            <el-option
+              v-for="(item, i) in plugins.typeOptions"
+              :key="i"
+              :label="item.label"
+              :value="item.value">
+            </el-option>
+          </el-select>
+        </el-form-item>
+      </el-form>
+    </el-drawer>
 
-    <el-table :data="compareDataFiltered">
-      <el-table-column
-        label="数字签名"
-        prop="hash"
-        sortable>
-      </el-table-column>
-      <el-table-column label="左侧文件名">
-        <template slot-scope="scope">
-          <template v-if="scope.row.lhs && scope.row.lhs.length > 0">
-            <div v-for="(item, i) in scope.row.lhs" :key="i">
-              {{item.filename}}
-            </div>
+    <div class="action-wrapper">
+      <el-button
+        size="mini"
+        type="info"
+        icon="el-icon-search"
+        circle
+        @click="popupFilterDrawer">
+      </el-button>
+    </div>
+
+    <div class="table-wrapper">
+      <el-table
+        class="table-component"
+        :data="compareDataFiltered"
+        size="mini"
+        border
+        height="auto">
+        <el-empty slot="empty" description="暂无数据"></el-empty>
+
+        <el-table-column
+          label="数字签名"
+          prop="hash"
+          sortable>
+        </el-table-column>
+        <el-table-column label="左侧文件名">
+          <template slot-scope="scope">
+            <template v-if="scope.row.lhs && scope.row.lhs.length > 0">
+              <div v-for="(item, i) in scope.row.lhs" :key="i">
+                {{item.filename}}
+              </div>
+            </template>
           </template>
-        </template>
-      </el-table-column>
-      <el-table-column label="" width="100">
-        <template slot-scope="scope">
-          <el-button
-            size="medium"
-            :type="scope.row.typeOption.type"
-            :icon="scope.row.typeOption.icon"
-            circle>
-          </el-button>
-        </template>
-      </el-table-column>
-      <el-table-column label="右侧侧文件名">
-        <template slot-scope="scope">
-          <template v-if="scope.row.rhs && scope.row.rhs.length > 0">
-            <div v-for="(item, i) in scope.row.rhs" :key="i">
-              {{item.filename}}
-            </div>
+        </el-table-column>
+        <el-table-column
+          width="100"
+          align="center">
+          <template slot-scope="scope">
+            <el-button
+              size="mini"
+              :type="scope.row.typeOption.type"
+              :icon="scope.row.typeOption.icon"
+              circle>
+            </el-button>
           </template>
-        </template>
-      </el-table-column>
-    </el-table>
+        </el-table-column>
+        <el-table-column label="右侧侧文件名">
+          <template slot-scope="scope">
+            <template v-if="scope.row.rhs && scope.row.rhs.length > 0">
+              <div v-for="(item, i) in scope.row.rhs" :key="i">
+                {{item.filename}}
+              </div>
+            </template>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
   </div>
 
   <script type="text/javascript" src="https://cdn.bootcdn.net/ajax/libs/vue/2.6.14/vue.min.js"></script>
@@ -68,6 +139,7 @@
       return {
         compareData: {{__ summary __}},
         state: {
+          filterDrawerVisible: false,
           filterTypes: ['add', 'remove', 'same'],
         },
         plugins: {
@@ -104,6 +176,11 @@
       }
     },
     methods: {
+      // popups / popdowns
+      popupFilterDrawer() {
+        this.state.filterDrawerVisible = true;
+      },
+      // Mappers
       applyTypeMapper(list = []) {
         let typeOptionMap = _.keyBy(this.plugins.typeOptions, 'value');
         let listMapped = _.map(list, v => {
